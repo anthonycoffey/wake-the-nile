d746df45e724eea645f0bc49b6381e161615ae52
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-06-28 12:13:30 -0500

    `v0.2.1`

---
+++ utils/zip.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const archiver = require('archiver');
+
+// Create a write stream for the zip file in the project root directory
+const output = fs.createWriteStream(path.join(__dirname, '..', 'wake-the-nile.zip'));
+const archive = archiver('zip', {
+  zlib: { level: 9 } // Sets the compression level.
+});
+
+output.on('close', function() {
+  console.log(archive.pointer() + ' total bytes');
+  console.log('archiver has been finalized and the output file descriptor has closed.');
+});
+
+archive.on('error', function(err) {
+  throw err;
+});
+
+archive.pipe(output);
+
+// append files from a directory, putting its contents at the root of the archive
+// These paths are relative to the project root, where the script is executed from.
+archive.directory('css/', 'css');
+archive.directory('js/', 'js');
+
+// append a file
+archive.file('index.php', { name: 'index.php' });
+
+archive.finalize();
