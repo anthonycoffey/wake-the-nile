b24354cbb4117fbcba8cae1a6d02af9472b533db
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-06-28 14:09:10 -0500

    `v0.3.10` - styling updates for video slider

--- wake-the-nile.php
+++ wake-the-nile.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Wake the Nile Custom WordPress Plugin
  * Description:       Custom WordPress Plugin to add features and extend the website.
- * Version:           0.3.9
+ * Version:           0.3.10
  * Author:            Anthony Coffey
  * Text Domain:       wake-the-nile
  * Domain Path:       /languages
@@ -17,7 +17,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants.
-define('WTN_VERSION', '0.3.9');
+define('WTN_VERSION', '0.3.10');
 define('WTN_PLUGIN_FILE', __FILE__);
 define('WTN_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('WTN_PLUGIN_URL', plugin_dir_url(__FILE__));
